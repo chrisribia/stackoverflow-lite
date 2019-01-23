6ed22a6243cb41f3641b083ddb33bd6b2ec755a6
chrisribia <chrisribia@gmail.com>
2019-01-23 11:54:49 +0300

    update question title

--- app/api/v1/routes.py
+++ app/api/v1/routes.py
@@ -1,10 +1,11 @@
 from flask import Blueprint
 from flask_restful import Api
 
-from .questions.views import  Quest,Quests
+from .questions.views import  Quest,Quests,UpdateTitle
 
 
 VERSION_ONE = Blueprint('api', __name__, url_prefix='/api/v1')
 API = Api(VERSION_ONE)
 API.add_resource(Quest, '/questions')
 API.add_resource(Quests, '/questions/<int:question_id>')
+API.add_resource(UpdateTitle, '/questions/<int:question_id>/title')
