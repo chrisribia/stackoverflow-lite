3453bcdfc7262633bf55f54a273fd175ba15d0d0
chrisribia <chrisribia@gmail.com>
2019-01-23 13:16:54 +0300

    add validators

--- app/__init__.py
+++ app/__init__.py
@@ -11,5 +11,6 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(APP_CONFIG[config_name])
     app.register_blueprint(v1)
+    app.url_map.strict_slashes = False
     
     return app
