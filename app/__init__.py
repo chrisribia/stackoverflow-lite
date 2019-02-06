a64c55e40f6027c3349f27ed245f88b440626d1c
chrisribia <chrisribia@gmail.com>
2019-02-06 12:37:12 +0300

    add

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,11 @@
 from flask import Blueprint, Flask
 
 from app.api.v1.routes import VERSION_ONE as v1
+from app.api.v2.routes import VERSION_DOS as v2
+
 
 from app.instance.config import APP_CONFIG
+from app.api.db_config import create_tables, super_user
 
 def create_app(config_name):
     '''The create_app function wraps the creation of a new Flask object,
@@ -12,7 +15,13 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(APP_CONFIG[config_name])# Load the default configuration
     app.url_map.strict_slashes = False
+    create_tables()
+    # super_user()
+
+
+
     app.register_blueprint(v1)
+    app.register_blueprint(v2)
     
     
     return app
